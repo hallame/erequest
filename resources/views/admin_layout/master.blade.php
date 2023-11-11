174dea56ae08c499242dbf0a46cbca366224d047
Hormise ALLAME <104220598+hallame@users.noreply.github.com>
2023-11-12 00:47:10 +0300

    admin master

--- resources/views/admin_layout/master.blade.php
+++ resources/views/admin_layout/master.blade.php
@@ -1,1 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>@yield('title')</title>
+          <!-- Stylesheets -->
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet">
+    <link href="{{asset('style/css/styles.css')}}" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+
+    <!-- Custom Styles -->
+    <style>
+        /* Style for the table */
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin: 20px 0;
+            font-size: 14px;
+        }
+        th, td {
+            padding: 10px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+        th {
+            background-color: #f2f2f2;
+            font-weight: bold;
+        }
+        tbody tr:hover {
+            background-color: #f5f5f5;
+        }
+
+        /* Style for the search bar */
+        #searchInput {
+            padding: 10px;
+            width: 100%;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            margin-bottom: 20px;
+        }
+
+        /* Style for buttons */
+        button {
+            padding: 10px 15px;
+            background-color: #007bff;
+            color: #fff;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            margin-right: 10px;
+        }
+        button:hover {
+            background-color: #0056b3;
+        }
+
+        /* Style for delete buttons (red color) */
+        button.delete {
+            background-color: #ff0000;
+        }
+        button.delete:hover {
+            background-color: #cc0000;
+        }
+    </style>
+        <link href="css/styles.css" rel="stylesheet" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="sb-nav-fixed">
+
+
+                {{-- Start header --}}
+                <!-- sidebar nav -->
+                @include('admin_layout.header')
+                {{-- Start header --}}
+
+
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading">Core</div>
+                            <a class="nav-link" href="index.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Dashboard
+                            </a>
+                            <div class="sb-sidenav-menu-heading">Interface</div>
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts" id="reclamationsLink">
+                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                RECLAMATIONS
+                                <!-- <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div> -->
+                            </a>
+
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts" id="bulletinLink">
+                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                BULLETIN
+                            </a>
+
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts" id="certificateLink">
+                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                ATTESTATIONS
+                            </a>
+
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                DIPLOMES
+                            </a>
+
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts" id="messageLink">
+                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                               MESSAGES
+                            </a>
+
+
+                        </div>
+                    </div>
+
+                </nav>
+            </div>
+            <div id="layoutSidenav_content">
+               
+            {{-- Start Content --}}
+                   @yield('content')
+            {{-- Start Content --}}
+                
+                <!-- start footer -->
+                @include('admin_layout.footer')        
+                <!-- end footer -->
+
+
+            </div>
+        </div>
+   
+            <!-- start scripts-->
+           @include('admin_layout.scripts')        
+           <!-- end scripts -->
+</body>
+</html>
 
